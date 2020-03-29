7ffaaa5d2ec30133d0b4bb08ccb20b0b71924232
arthur <arthur.saint-denis@epita.fr>
2020-03-29 17:33:28 +0200

    removes useless print

--- commands/pnj/subcommands/addPnj.js
+++ commands/pnj/subcommands/addPnj.js
@@ -8,7 +8,6 @@ async function addPnj(msg, ast) {
   if (msg.attachments.size > 1) throw "Trop de fichiers joint au message";
   if (msg.attachments.size === 1) {
     const pic = msg.attachments.array()[0];
-    console.log(pic);
     pnj.picture = pic.url;
   }
 
