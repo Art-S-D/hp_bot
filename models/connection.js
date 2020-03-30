f5303de05648c8494606fc0fb1f00b7d3291601b
arthur <arthur.saint-denis@epita.fr>
2020-03-30 20:33:11 +0200

    removes a mongoose error message

--- models/connection.js
+++ models/connection.js
@@ -2,5 +2,6 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/dev", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true
 });
