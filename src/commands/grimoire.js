e41912fbd4ef244735f0249ebe4ba53bf4cf1550
arthur <arthur.saint-denis@epita.fr>
2020-04-19 11:59:33 +0200

    renames nom to name

--- src/commands/grimoire.js
+++ src/commands/grimoire.js
@@ -1,22 +1,22 @@
 const { Player } = require("mongo");
 
 function print_grimoire(player) {
-    const { nom, ..._player } = player;
-    let res = nom + "\n";
-    for (i in _player) {
-        res += i + "---\n";
-        for (j in _player[i]) {
-            res += j + ": " + _player[i][j] + "\n";
-        }
-        res += "\n";
+  const { name, ..._player } = player;
+  let res = name + "\n";
+  for (i in _player) {
+    res += i + "---\n";
+    for (j in _player[i]) {
+      res += j + ": " + _player[i][j] + "\n";
     }
-    return res;
+    res += "\n";
+  }
+  return res;
 }
 
 async function grimoire(msg) {
-    const player = await Player.findOne({ nom: msg.member.nickname });
-    if (player) msg.reply(print_grimoire(player));
-    else msg.reply("joueur inconnu");
+  const player = await Player.findOne({ name: msg.member.nickname });
+  if (player) msg.reply(print_grimoire(player));
+  else msg.reply("joueur inconnu");
 }
 
 grimoire.critical = true;
