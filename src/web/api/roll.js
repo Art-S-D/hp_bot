16f60f0a85b3ebaa381c3f674883c400dbef8b2a
arthur <arthur.saint-denis@epita.fr>
2020-04-20 20:02:25 +0200

    fix the backend to only store the 10 last rolls

--- src/web/api/roll.js
+++ src/web/api/roll.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express.Router();
 
-const rolls = [];
+const STORE_LAST = 10;
+let rolls = [];
+
 app.get("/latest", async function (req, res) {
   /*const tmp = req.param.date
     ? rolls.filter((x) => x.date.getTime() > req.body.date)
     : rolls;*/
   const tmp = rolls.sort((a, b) => a.date - b.date);
-  res.status(200).send(tmp.slice(-10));
+  res.status(200).send(tmp);
 });
 
 app.post("/new", async function (req, res) {
@@ -16,6 +18,7 @@ app.post("/new", async function (req, res) {
     name: req.user.name,
     value: Math.ceil(Math.random() * 20),
   });
+  rolls = rolls.slice(-STORE_LAST);
   res.status(200).end();
 });
 module.exports = app;
